9246863184d08f7a8d0a2e3029783e03dbc9f816
aryan-chanana <93636611+aryan-chanana@users.noreply.github.com>
2021-11-16 23:43:33 +0530

    Add files via upload

---
+++ Name.py
@@ -0,0 +1,11 @@
+print("welcome! I am Aryan")
+name = input("enter your name")
+print("Hello"+name)
+age = int(input("Enter your age")
+        if age < 15:
+          print("enjoy your life")
+          elif age > 85:
+          print("wait for your death")
+          else:
+          print("work hard")
+input()
