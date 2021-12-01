50fd1dd81879e4efc155ad64e8db38d9282eb160
aryan-chanana <93636611+aryan-chanana@users.noreply.github.com>
2021-12-01 12:23:37 +0530

    Update group_project.c

--- group_project.c
+++ group_project.c
@@ -3,26 +3,27 @@
 #include <time.h>
 
 
-// input vehicle type, subcategory, model, vehicle no, owner contact name, contact no
+// input vehicle type, subcategory, model, vehicle no, owner name, contact no
 
 struct vehicle{
     int type;
     int subcategory;
     char model[20];
-    char vehicle_no[10];
+    char vehicle_no[15];
     char name[20];
-    char contact_no[10];
+    char contact_no[15];
 };
+
+struct vehicle user;
 
 int entry();
 int two_wheeler();
 int four_wheeler();
 int commercial();
+int user_Details();
+int entry_receipt();
 
 int main() {
-    time_t t;
-    time(&t);
-    //printf("%s\n", ctime(&t));
     int start;
     printf("\t\t***WELCOME TO GRAND LEVEL PARKING***\n\n");
     printf("What do you want to do?\n");
@@ -40,14 +41,15 @@ int main() {
 }
 
 int entry() {
-    struct vehicle user;
+    //int type;
+    int total=10;
     printf("Select vehicle type\n");
     printf("1. Two wheeler\n");
     printf("2. Four wheeler\n");
-    printf("3. Commercial Transport Vehicle:");
+    printf("3. Commercial Transport Vehicle: ");
     scanf("%d",&user.type);
     if (user.type==1){
-        //two_wheeler();
+        two_wheeler();
     }
     else if (user.type==2) {
         //four_wheeler();
@@ -55,4 +57,41 @@ int entry() {
     else if (user.type==3) {
         //commercial();
     }
+}
+
+int two_wheeler() {
+    int sub;
+    printf("\n Cost is Rs. 10/hr Each\n");
+    printf("Select sub-category\n");
+    printf("1. Cylce\n");
+    printf("2. Scooty\n");
+    printf("3. Bike: ");
+    scanf("%d",&sub);
+    user_Details();
+    printf("Here is your Entry receipt...\n\n");
+    entry_receipt();
+}
+
+int user_Details() {
+    //char name[20], contact[10], vehicle_no[10], model[20];
+    printf("\nEnter name: ");
+    scanf("%s",user.name);
+    printf("Enter contact no: ");
+    scanf("%s",user.contact_no);
+    printf("Enter vehicle no: ");
+    scanf("%s",user.vehicle_no);
+    printf("Enter model: ");
+    scanf("%s",user.model);
+}
+
+int entry_receipt() {
+    time_t t;
+    time(&t);
+    printf("***************************************\n");
+    printf("%s\n", ctime(&t));
+    printf("Owner name: %s\n",user.name);
+    printf("Owner contact no: %s\n",user.contact_no);
+    printf("Vehicle no: %s\n",user.vehicle_no);
+    printf("Vehicle model: %s\n",user.model);
+    printf("***************************************");
 }
